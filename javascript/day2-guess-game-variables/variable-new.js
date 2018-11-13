a62a9ba3c66ed38af42c6c3082bdf2e21d1b7826
Jehu Shalom <jehuamanna@gmail.com>
2018-11-13 14:09:21 +0530

    day 2

---
+++ javascript/day2-guess-game-variables/variable-new.js
@@ -0,0 +1,18 @@
+//defining a variable using the keyword using the keyword var will allow us to change the value in the future.
+var maxSpeed = 100
+console.log(maxSpeed)
+
+maxSpeed = 150
+console.log(maxSpeed)
+
+//es6 - ecmascript 2015 
+//defining a variable using the keyword const will not allow us to change the value in the future
+
+const maxRounds = 5
+//maxRounds = 10
+
+//defining a vaiable using the keyword var will allow us to change the value in the future
+let count = 0
+console.log(count)
+count++
+console.log(count)
---
+++ javascript/day2-guess-game-variables/variable.js
@@ -0,0 +1,32 @@
+var name = 'ani'
+
+//if  no value is assigned, by default a variable holds undefined
+var user
+console.log(user)
+
+// string type
+var city = 'bangalore'
+console.log(city)
+
+//number type
+var price = 12
+console.log(price)
+
+//arry type
+var numbers = [10,20,30]
+console.log(numbers)
+
+//object type
+
+var person = { 
+    firstName: 'arjun'
+}
+console.log(person)
+
+//function
+//anonymous function
+var greet = function() {
+    console.log('hello there')
+}
+console.log(greet)
+greet()
