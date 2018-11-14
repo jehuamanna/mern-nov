9f4afe56c0eeb73fba92bf1be8911d15ea9bb87f
Jehu Shalom <jehuamanna@gmail.com>
2018-11-14 11:27:39 +0530

    plus operator

---
+++ javascript/day3-numbers-string/plus-operator.js
@@ -0,0 +1,11 @@
+console.log('hello ' + 'world') // 'hello world'
+console.log(5 + 5 ) //10
+console.log('5' + 6) //56
+console.log(5 + '7') //57
+console.log('1' + '2'+ 3) //123
+console.log(2 + 4+ '5') //65
+console.log('4' + 2 + 6) //426
+console.log('1' + 5 + '2') //152
+
+//48
+console.log('4' + (2+6)) //
