923885b392059e38cfc726f832c7e85f13951f75
Jehu Shalom <jehuamanna@gmail.com>
2018-11-30 11:09:30 +0530

    changes

---
+++ javascript/day15-conditions/async.js
@@ -0,0 +1,19 @@
+console.log('user 1 requesting product information')
+setTimeout(function(){
+    console.log('user 1 response sent')
+}, 1500)
+
+console.log('user 2 requesting orer information')
+setTimeout(function(){
+    console.log('user 2 respnse sent')
+}, 2000)
+
+console.log('user 3 requesting shoe info')
+setTimeout(function(){
+    console.log('user 3 response sent')
+}, 500)
+
+console.log('user 4 requesting login screen')
+setTimeout(function(){
+    console.log('user 4 response sent')
+}, 200)
