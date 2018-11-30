cd0d84a0bd57528265bf88b4e8ce328659ce4853
Jehu Shalom <jehuamanna@gmail.com>
2018-11-30 10:56:04 +0530

    removed tokens

--- javascript/day14-axios/secret/get-temperature.js
+++ javascript/day14-axios/secret/get-temperature.js
@@ -5,19 +5,15 @@ latLng = {}
 axios.get(url).then(function(response){
     latLng.lat = response.data.results[0].locations[0].latLng.lat
     latLng.lng = response.data.results[0].locations[0].latLng.lng
+    let url = `https://api.darksky.net/forecast/ba4a488b3ec19f6c8c970c7bcfa5dfc8/${latLng.lat},${latLng.lng}`
+    console.log(url)
+    axios.get(url).then(function(response){
+        data = response.data
+        temperature = data.currently.temperature
+        temperature = (temperature -32)*5/9
+        console.log(temperature)
+    })
 }).catch(function(err){
     console.log(err.message)
 })
 
-console.log(latLng)
-
-url = `https://api.darksky.net/forecast/ba4a488b3ec19f6c8c970c7bcfa5dfc8/${latLng.lat},${latLng.lng}`
-console.log(url)
-axios.get(url).then(function(response){
-    data = response.data
-    temperature = data.currently.temperature
-    temperature = (temperature -32)*5/9
-    console.log(temperature)
-}).catch(function(err){
-    console.log(err.message)
-})
