52f623a22f62007dc40ce54a061ca1aee953a62c
Jehu Shalom <jehuamanna@gmail.com>
2018-12-19 22:46:20 +0530

    commit

---
+++ javascript/day25-traversing-dom-ajax/movie.js
@@ -0,0 +1,22 @@
+const movie = [
+    {
+        name: 'Kill Bill',
+        summary: 'After awakening from a four-year coma, a former assasin wreaks vengence on the team of assassins who betrayed her',
+        director : [
+            { 
+                name:'Quentin Tarantino'
+            }
+        ],
+        actors: [
+            {
+                name: 'Uma Thurman'
+            },
+            {
+                 name:'david'
+            },
+            {
+                name:'Daryl'
+            }
+        ]
+    }
+]
