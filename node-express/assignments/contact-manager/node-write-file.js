3691515624978af3ee03765bc3bd5f37000a504e
Jehu Shalom <jehuamanna@gmail.com>
2019-01-02 12:34:22 +0530

    started project

---
+++ node-express/assignments/contact-manager/node-write-file.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+console.log("Going to write into existing file");
+contacts = [{id:1, name:'jehu', contact: '9008141866'}]
+fs.writeFile('input.txt', JSON.stringify(contacts), function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   
+   fs.readFile('input.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+   });
+});
