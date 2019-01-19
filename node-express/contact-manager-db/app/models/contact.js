07e950d38f1c01daa2fa024cf0b30b99d9c76106
Jehu Shalom <jehuamanna@gmail.com>
2019-01-19 08:31:10 +0530

    added comments

--- node-express/contact-manager-db/app/models/contact.js
+++ node-express/contact-manager-db/app/models/contact.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+//common terminologies in MONGO DB
+// DB - nothing but a software used to store data
+// Collection - [{},{},{}]
+// Document - {} regular javascript obect
+// Field - nothing but a property
+
+// to store information about a contact - like the name, email, phone, website
+// const contacts = [{id:1, name:'ani',email: 'ani@gmail.com', phone 1234, website: 'www.ab.com'}]
+
+// contacts = Collection
+//{id:1, name:'ani',email: 'ani@gmail.com', phone 1234, website: 'www.ab.com'} = Document
+// id = field
+// name = field
+// mobile, email, website = field
+
 
 const { Schema } = mongoose
 const contactSchema = new Schema({
