57674d9a6b9ae4c412a851039e77bb35e365f322
Jehu Shalom <jehuamanna@gmail.com>
2019-01-19 08:30:42 +0530

    removed comments

--- node-express/contact-manager-db/index.js
+++ node-express/contact-manager-db/index.js
@@ -8,20 +8,6 @@ app.use(express.json())
 
 
 
-//common terminologies in MONGO DB
-// DB - nothing but a software used to store data
-// Collection - [{},{},{}]
-// Document - {} regular javascript obect
-// Field - nothing but a property
-
-// to store information about a contact - like the name, email, phone, website
-// const contacts = [{id:1, name:'ani',email: 'ani@gmail.com', phone 1234, website: 'www.ab.com'}]
-
-// contacts = Collection
-//{id:1, name:'ani',email: 'ani@gmail.com', phone 1234, website: 'www.ab.com'} = Document
-// id = field
-// name = field
-// mobile, email, website = field
 
 
 
