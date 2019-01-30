fd35c6848fdcb15d26a4c955b8b4d2f1d062cbe1
Jehu Shalom <jehuamanna@gmail.com>
2019-01-30 12:34:59 +0530

    changes in database connection

--- node-express/assignments/quotify/config/database.js
+++ node-express/assignments/quotify/config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/quotify>' 
 
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/quotify')
+// mongoose.connect('mongodb://localhost:27017/quotify')
+mongoose.connect(CONNECTION_URI)
     .then((result) => {
         console.log('connected to db')
     })
